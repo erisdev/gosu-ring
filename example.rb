d2e3354d800ece945992c519b5df792f68a6ef74
Eris <eris.discord@gmail.com>
2010-05-02 01:37:48 -0400

    draws a ring menu with no animation and basic block actions

--- example.rb
+++ example.rb
@@ -3,6 +3,18 @@ require 'ring_menu'
 class RingDemo < Chingu::Window
   def initialize
     super 400, 200, false
+    
+    icons = Gosu::Image.load_tiles(self, 'data/icons.png', 16, 16, false)
+    
+    menu = RingMenu.new do |m|
+      m.item('Quit',   icons[0]) { close }
+      m.item('Plus',   icons[1])
+      m.item('Minus',  icons[2])
+      m.item('Coffee', icons[3])
+    end
+    
+    push_game_state menu
+    
   end
 end
 
--- lib/ring_menu.rb
+++ lib/ring_menu.rb
@@ -1,5 +1,75 @@
 require 'chingu'
 
 class RingMenu < Chingu::GameState
+  class Icon < Chingu::GameObject
+    attr_accessor :title
+    
+    def initialize title, image, options = {}, &block
+      super options.merge(:image => image)
+      @title  = title
+      @action = block
+      puts "Created #{self} (#{title}) -> #{block}"
+    end
+    
+    def perform_action
+      @action[self]
+    end
+  end
+  
+  DEFAULTS = { :radius => 50 }
+  
+  def initialize options = {}, &block
+    super
+    
+    options = DEFAULTS.merge options
+    @radius = options[:radius]
+    
+    @items = []
+    @index = 0
+    @step  = 0
+    
+    self.input = {
+      :left  => :left!,
+      :right => :right!,
+      :return => :act!
+    }
+    
+    yield self if block_given?
+  end
+  
+  def item caption, image, options = {}, &block
+    # FIXME when chingu is updated, this should just use the create method
+    @items << Icon.new(caption, image, options, &block)
+    add_game_object @items.last
+  end
+  
+  def left!
+    @index -= 1
+    @index  = @items.count - 1 if @index < 0
+  end
+  
+  def right!
+    @index += 1
+    @index  = 0 if @index >= @items.count
+  end
+  
+  def act!
+    @items[@index].perform_action
+  end
+  
+  def update
+    super
+    
+    cx = $window.width / 2
+    cy = $window.height / 2
+    
+    angle_step = 2 * Math::PI / @items.count
+    angle      = angle_step * @index
+    @items.each do |icon|
+      icon.x = cx + @radius * Math.sin(angle)
+      icon.y = cy - @radius * Math.cos(angle)
+      angle += angle_step
+    end
+  end
   
 end
