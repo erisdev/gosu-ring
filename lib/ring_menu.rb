aa483e5578fa8478ec4ded3b03b781f23fa70457
Eris <eris.discord@gmail.com>
2010-05-02 02:04:59 -0400

    animate ring rotation

--- lib/ring_menu.rb
+++ lib/ring_menu.rb
@@ -8,12 +8,12 @@ class RingMenu < Chingu::GameState
       super options.merge(:image => image)
       @title  = title
       @action = block
-      puts "Created #{self} (#{title}) -> #{block}"
     end
     
     def perform_action
       @action[self]
     end
+    
   end
   
   DEFAULTS = { :radius => 50 }
@@ -41,16 +41,23 @@ class RingMenu < Chingu::GameState
     # FIXME when chingu is updated, this should just use the create method
     @items << Icon.new(caption, image, options, &block)
     add_game_object @items.last
+    @count = @items.count
   end
   
   def left!
     @index -= 1
-    @index  = @items.count - 1 if @index < 0
+    if @index < 0
+      @index += @count
+      @step  += @count
+    end
   end
   
   def right!
     @index += 1
-    @index  = 0 if @index >= @items.count
+    if @index >= @count
+      @index -= @count
+      @step  -= @count
+    end
   end
   
   def act!
@@ -60,15 +67,25 @@ class RingMenu < Chingu::GameState
   def update
     super
     
+    # update rotation step
+    if (@step - @index).abs < 0.1
+      @step = @index
+    elsif @step < @index
+      @step += 0.1
+    elsif @step > @index
+      @step -= 0.1
+    end
+    
     cx = $window.width / 2
     cy = $window.height / 2
     
-    angle_step = 2 * Math::PI / @items.count
-    angle      = angle_step * @index
+    angle_diff = 2 * Math::PI / @count
+    this_angle = angle_diff * @step
+    
     @items.each do |icon|
-      icon.x = cx + @radius * Math.sin(angle)
-      icon.y = cy - @radius * Math.cos(angle)
-      angle += angle_step
+      icon.x = cx + @radius * Math.sin(this_angle)
+      icon.y = cy - @radius * Math.cos(this_angle)
+      this_angle += angle_diff
     end
   end
   
