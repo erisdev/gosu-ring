b5987589f34caf724fe86da85d4005352c3866d8
Eris <eris.discord@gmail.com>
2010-05-07 18:15:41 -0400

    separated RingMenu::Icon into its own file

--- lib/ring_menu.rb
+++ lib/ring_menu.rb
@@ -1,23 +1,7 @@
 require 'chingu'
 
 class RingMenu < Chingu::GameState
-  class Icon < Chingu::GameObject
-    attr_accessor :title
-    
-    def initialize title, image, options = {}, &block
-      super options.merge(:image => image, :center => 0.5)
-      @title  = title
-      @action = block
-      
-      # make actionless icons transparent while preserving color
-      @color.alpha /= 2 unless @action
-    end
-    
-    def perform_action
-      @action[self] if @action
-    end
-    
-  end
+  autoload :Icon, 'ring_menu/icon'
   
   module Z
     BACKGROUND = 0
